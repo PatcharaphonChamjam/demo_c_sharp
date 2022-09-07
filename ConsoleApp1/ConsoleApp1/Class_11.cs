99e215e1ff10d44b92474f9a72e9c3a13a09cea2
TPA-NB-COM-27\TPA-NB-COM-27 <patcharaphon.chamjam@gmail.com>
2022-09-07 16:52:38 +0700

    Chapter 11

--- ConsoleApp1/ConsoleApp1/Class_11.cs
+++ ConsoleApp1/ConsoleApp1/Class_11.cs
@@ -8,6 +8,42 @@ namespace ConsoleApp1
 {
     public class Class_11
     {
-       
+        public int energyUserPerRun =3;
+        //String color;
+
+        public float enegy =10;
+        public int speed = 0;
+        public int distance = 0;
+
+       // public Car(string c,float e,int s,int d)
+        //{
+            //color = c;
+            //enegy = e;
+            //speed = s;
+            //distance = d;
+        //}
+
+        public void AddEnegy()
+        {
+            enegy += 10;
+        }
+
+        public void Run ()
+        {
+            if(enegy < speed * energyUserPerRun)
+            {
+                Console.WriteLine("No enough enegy to run.");
+            }
+            else
+            {
+                distance += (speed * energyUserPerRun);
+                enegy -= energyUserPerRun;
+            }
+        }
+
+        public void ShowDistance()
+        {
+            Console.WriteLine("Distance runned "+distance);
+        }
     }
 }

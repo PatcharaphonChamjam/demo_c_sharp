4c9536f41dd9a8114a6ec4317095fbb7f9e5c55d
TPA-NB-COM-27\TPA-NB-COM-27 <patcharaphon.chamjam@gmail.com>
2022-09-08 08:58:19 +0700

    Chapter 12 Object : Add Comment variable

--- ConsoleApp1/ConsoleApp1/Objects_12.cs
+++ ConsoleApp1/ConsoleApp1/Objects_12.cs
@@ -8,8 +8,11 @@ namespace ConsoleApp1
 {
     public class Objects_12
     {
+        //กำหนดตัวแปร name Type string ,public
         public string name;
+        //กำหนดตัวแปร width Type int ,public
         public int width;
+        //กำหนดตัวแปร height Type int ,public
         public int height;
 
         public void setDimension(int w,int h)

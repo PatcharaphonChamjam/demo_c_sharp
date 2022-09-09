e95e5fd39092b32f34732dd041aef847de11fc19
TPA-NB-COM-27\TPA-NB-COM-27 <patcharaphon.chamjam@gmail.com>
2022-09-09 14:27:59 +0700

    clean square and rectangle

--- Demo_Square/Demo_Square/Program.cs
+++ Demo_Square/Demo_Square/Program.cs
@@ -14,27 +14,20 @@ namespace Demo_Square
             try
             {
                 //square
-                double lengh;
-
                 Square square = new Square();
 
                 Console.Write("Enter value lengh of square : ");
-                /*lengh = double.Parse(Console.ReadLine())*/
-                ;
                 square.SetDimension(double.Parse(Console.ReadLine()));
 
                 Console.WriteLine("Formula is " + square.lenght + " * " + square.lenght + " The area of the square is : " + square.Calculate());
 
                 //rectangle
-                double width;
-                double height;
-
                 Rectangle rectangle = new Rectangle();
 
                 Console.Write("Enter value width of rectangle : ");
-                width = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
                 Console.Write("Enter value height of rectangle : ");
-                height = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
 
                 rectangle.SetDimension(width, height);
                 Console.WriteLine("Formula is " + rectangle.width + " * " + rectangle.height + " The area of the square is : " + rectangle.Calculate());

d4ddfe9be60e473c89c33722425db50da7a20e79
TPA-NB-COM-27\TPA-NB-COM-27 <patcharaphon.chamjam@gmail.com>
2022-09-09 12:00:26 +0700

    Create project

--- Demo_Square/Demo_Square/Program.cs
+++ Demo_Square/Demo_Square/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Demo_Square
 {
@@ -6,7 +10,6 @@ namespace Demo_Square
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }

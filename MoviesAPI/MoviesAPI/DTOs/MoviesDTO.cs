6899c66f13fe9a11ba43edebf1806adc734e7a59
TPA-NB-COM-27\TPA-NB-COM-27 <patcharaphon.chamjam@gmail.com>
2022-09-19 23:46:03 +0700

    feat: Movie Entity

---
+++ MoviesAPI/MoviesAPI/DTOs/MoviesDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MoviesAPI.DTOs
+{
+    public class MoviesDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public bool InTheaters { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
